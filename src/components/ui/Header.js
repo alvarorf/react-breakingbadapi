cc17f4f8a2acc7007029805b41f0c9450d932166
alvaro <aaruizf@unal.edu.co>
2020-07-13 19:26:06 -0400

    Added useState and useEffect hooks to fetch the data (in JSON format)

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import logo from '../../img/logo.png'
 const header = () => {
     return (
-        <div>
-            
-        </div>
+        <header className='center'>
+            <img src={logo} alt='' />
+
+        </header>
     )
 }
 
