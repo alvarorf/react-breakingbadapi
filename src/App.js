159d4607cc9453330963556b916fd31534309cde
alvaro <aaruizf@unal.edu.co>
2020-07-14 16:40:15 -0400

    Added some hooks and created CharacterGrid component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Header from './components/ui/Header'
+import CharacterGrid from './components/characters/CharacterGrid'
 import './App.css';
 
 const App = () => {
-  const [item, setItems] = useState([]) 
+  const [items, setItems] = useState([]) 
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
@@ -20,6 +21,7 @@ const App = () => {
   return (
   <div className="container">
     <Header />
+    <CharacterGrid isLoading={isLoading} items={items}/>
   </div>
   )
 }
